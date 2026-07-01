fb3cb2129bf629f42faf7736fe3a65e91b13f0da
hungsean <hungsean0419@gmail.com>
2026-07-01 15:52:54 +0800

    test cron

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import cron from 'node-cron';
 
+// #region init area
 const app = new Hono()
+let count = 0;
+const crontask = cron.schedule("* * * * * *", () => {
+  console.log("count: ", count);
+  count += 1;
+}, {
+  name: "test"
+})
+crontask.stop();
+// #endregion
+
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text(`Hello Hono! count: ${count}`)
 })
 
 serve({
